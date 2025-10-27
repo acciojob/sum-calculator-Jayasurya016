ae08dda712a6787f52a3a0c1c2d4cb47d997f450
JAYASURYA KADAKUZHILL JOB <161327970+Jayasurya016@users.noreply.github.com>
2025-10-27 10:51:14 +0000

    updated

--- src/components/SumCalculator.js
+++ src/components/SumCalculator.js
@@ -1,13 +1,17 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 export default function SumCalculator() {
 
   const [count, setCount] = useState(0)
 
+  // useEffect(()=>{
+  //   count=
+  // })
+
   return (
     <div>
       <h1>Sum Calculator</h1>
-      <input type="number" onClick={(e) => {
+      <input type="number" onChange={(e) => {
         setCount(count + Number(e.target.value))
       }} />
       <p>Sum: {count}</p>
